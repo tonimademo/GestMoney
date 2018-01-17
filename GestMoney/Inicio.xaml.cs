118f26538c475b0fa5eb991ace1c8be473f2e963
tonimademo <tonimademo@gmail.com>
2018-01-17 21:26:39 +0100

    Binding

--- GestMoney/Inicio.xaml.cs
+++ GestMoney/Inicio.xaml.cs
@@ -13,6 +13,9 @@ namespace GestMoney
     /// <summary>
     /// Lógica de interacción para Inicio.xaml
     /// </summary>
+    /// 
+
+
     public partial class MainWindow : Window
     {
         private DataTable _datafill = new DataTable();
@@ -30,21 +33,23 @@ namespace GestMoney
                 }
                 
                 }
-                set
-                {
+        set {
                 _datafill = value.Table;
             }
         }
+        
 
-        public MainWindow()
+        public  MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+
         }
 
         private void Inicio_Loaded(object sender, RoutedEventArgs e)
         {
             //var data = ServicioFactura.vRecibosSelect(ref datafill);
-            
+
             //if (ServicioFactura.vRecibosSelect(ref datafill).Key)
             //{
             //    datafill.DefaultView.RowFilter = "1=1";
@@ -52,10 +57,9 @@ namespace GestMoney
             //    //ColorRows();
 
             //}
-
+            _datafill.DefaultView.RowFilter = "1=1";
         }
-
-    
+        
 
         private void txtFiltroDesde_SelectedDateChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
@@ -66,9 +70,10 @@ namespace GestMoney
 
             //Si he quitado el valor y lo dejo vacio, no tiene que aplicar el campo al filtro y lo tengo que quitar por lo que paso una exp reg para saber que quitar
             Funciones.ModificarFiltro(ref _datafill, _datafill.DefaultView.RowFilter, filtro_local, regex);
-            
+
             //Aplico el nuevo filtro
             //dgvRecibos.ItemsSource = datafill.DefaultView;
+            Datafill = _datafill.DefaultView;
         }
 
         private void txtFiltroHasta_SelectedDateChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
@@ -83,6 +88,7 @@ namespace GestMoney
 
             //Aplico el nuevo filtro
             //dgvRecibos.ItemsSource = datafill.DefaultView;
+            Datafill = _datafill.DefaultView;
         }
         
         private void txtImporteMayor_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
---
+++ GestMoney/ViewModel/vmInicion.cs
@@ -0,0 +1,176 @@
+﻿using GestMoney.Clases;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace GestMoney.ViewModel
+{
+    class vmInicion : ObservableCollection<Factura>, INotifyPropertyChanged
+    {
+        #region Atributos
+        private int selectedIndex;
+
+        private int id;
+        private string tipo;
+        private string concepto;
+        private decimal importe;
+        private DateTime fecha_importe;
+        private DateTime gc_fecha_creacion;
+        private DateTime gc_fecha_modificacion;
+        private ICommand addClientCommand;
+        private ICommand clearCommand;
+        #endregion
+
+        #region Propiedades
+        public int SelectedIndexOfCollection
+        {
+            get
+            {
+                return selectedIndex;
+            }//Fin de get.
+            set
+            {
+                selectedIndex = value;
+                OnPropertyChanged("SelectedIndexOfCollection");
+
+                //Activa el evento OnPropertyChanged de los atributos para refrescar las propiedades segun el indice seleccionado.
+                OnPropertyChanged("Id");
+                OnPropertyChanged("Name");
+                OnPropertyChanged("LastName");
+            }//Fin de set.
+        }//Fin de propiedad Name.
+
+        public int Id
+        {
+            get
+            {
+                if (this.SelectedIndexOfCollection > -1)
+                {
+                    return this.Items[this.SelectedIndexOfCollection].Id;
+                }
+                else
+                {
+                    return id;
+                }
+            }
+            set { }
+        }
+
+        public string Tipo
+        {
+            get
+            {
+                if (this.SelectedIndexOfCollection > -1)
+                {
+                    return this.Items[this.SelectedIndexOfCollection].Tipo;
+                }
+                else
+                {
+                    return tipo;
+                }
+            }//Fin de get.
+            set
+            {
+                if (this.SelectedIndexOfCollection > -1)
+                {
+                    this.Items[this.SelectedIndexOfCollection].Tipo = value;
+                }
+                else
+                {
+                    tipo = value;
+                }
+                OnPropertyChanged("Tipo");
+            }
+        }
+
+        public string Concepto
+        {
+            get
+            {
+                if (this.SelectedIndexOfCollection > -1)
+                {
+                    return this.Items[this.SelectedIndexOfCollection].Concepto;
+                }
+                else
+                {
+                    return concepto;
+                }
+            }
+            set
+            {
+                if (this.SelectedIndexOfCollection > -1)
+                {
+                    this.Items[this.SelectedIndexOfCollection].Concepto = value;
+                }
+                else
+                {
+                    concepto = value;
+                }
+                OnPropertyChanged("LastName");
+            }
+        }
+
+        public ICommand AddClientCommand
+        {
+            get
+            {
+                return addClientCommand;
+            }//Fin de get.
+            set
+            {
+                addClientCommand = value;
+            }//Fin de set.
+        }//Fin de propiedad LastName.
+
+        public ICommand ClearCommand
+        {
+            get
+            {
+                return clearCommand;
+            }//Fin de get.
+            set
+            {
+                clearCommand = value;
+            }//Fin de set.
+        }//Fin de propiedad LastName.
+        #endregion
+
+        #region Constructores
+        public vmInicion()
+        {
+            //AddClientCommand = new CommandBase(param => this.AddClient());
+            //ClearCommand = new CommandBase(new Action<Object>(ClearClient));
+        }
+        #endregion
+
+        #region Interface
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        #region Metodos/Funcciones
+        private void AddClient()
+        {
+            
+        }
+
+        private void ClearClient(object obj)
+        {
+            
+        }
+        #endregion
+
+    }
+}
